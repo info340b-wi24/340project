41d515401d721b6b3b3ca545c47b41f55e469cb7
aryannn108 <75058883+aryannn108@users.noreply.github.com>
2024-02-26 18:21:51 -0800

    reactyyy

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
 import './index.css';
 import './css/combined.css'; // Import the global stylesheet
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+createRoot(
+  document.getElementById('root')
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
