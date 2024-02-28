4d8fc66cd31ebf06fa015e3fcf26f33e3dc4d35e
aryannn108 <75058883+aryannn108@users.noreply.github.com>
2024-02-27 20:59:58 -0800

    Aryan set up firebase hosting setup

--- src/index.js
+++ src/index.js
@@ -6,6 +6,25 @@ import './css/combined.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKnMX8Gz8WGVzZw3Y-7rfLTvSB62c3s2Y",
+  authDomain: "subletcentral.firebaseapp.com",
+  projectId: "subletcentral",
+  storageBucket: "subletcentral.appspot.com",
+  messagingSenderId: "1014839689452",
+  appId: "1:1014839689452:web:084a37ad1168c79e99a5c9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createRoot(
   document.getElementById('root')
 ).render(
