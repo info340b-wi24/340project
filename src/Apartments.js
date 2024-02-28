682ae07c5eea131169c2675e5ff9f79d218aec56
arushiarushi <agar.arushi@gmail.com>
2024-02-27 18:25:49 -0800

    Arushi restored apartment cards

--- src/Apartments.js
+++ src/Apartments.js
@@ -20,7 +20,7 @@ function Apartments() {
                                     <p><span className="bold-text black-text">Duration:</span> June - August 2024</p>
                                 </div>
                             </a> 
-                            <a href="apartment1.html" className="card-link">
+                            <a href="apartment2.html" className="card-link">
                                 <div className="card">
                                     <img src="img/hub.jpg" alt="This is an image of a bedroom at the Hub Apartments." />
                                     <h2>1 Bed @ Hub Apartments</h2>
@@ -28,7 +28,31 @@ function Apartments() {
                                     <p><span className="bold-text black-text">Duration:</span> Jan - March 2024</p>
                                 </div>
                             </a> 
-                            {/* Continue with other card components similarly */}
+                            <a href="apartment3.html" className="card-link">
+                                <div className="card">
+                                    <img src="img/nolan.jpg" alt="This is an image of a bedroom at the Nolan Apartments." />
+                                    <h2>1 Bed @ Nolan Apartments</h2>
+                                    <p><span className="bold-text black-text">Rent:</span> $1100 per month</p>
+                                    <p><span className="bold-text black-text">Duration:</span> June - August 2024</p>
+                                </div>
+                            </a> 
+                            <a href="apartment4.html" className="card-link">
+                                <div className="card">
+                                    <img src="img/kelsey.jpg" alt="This is an image of a bedroom at the Kelsey." />
+                                    <h2>1 Bed @ Kelsey</h2>
+                                    <p><span className="bold-text black-text">Rent:</span> $800 per month</p>
+                                    <p><span className="bold-text black-text">Duration:</span> April - August 2024</p>
+                                </div>
+                            </a>
+                            <a href="apartment5.html" className="card-link">
+                                <div className="card">
+                                    <img src="img/twelve.jpg" alt="This is an image of a bedroom at the Twelve Apartments." />
+                                    <h2>1 Shared Bedroom @ Twelve Apartments</h2>
+                                    <p><span className="bold-text black-text">Rent:</span> $1600 per month</p>
+                                    <p><span className="bold-text black-text">Duration:</span> June - August 2024</p>
+                                </div>
+                            </a>
+                            {/* Add more card components for additional apartments */}
                         </div>
                     </section>
                 </div>
