9fd8e17338604ef209fef98925f3bf8995c5a418
arushiarushi <agar.arushi@gmail.com>
2024-03-11 18:05:48 -0700

    fixed the error message

--- src/ApartmentDetail.js
+++ src/ApartmentDetail.js
@@ -1,20 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getDatabase, ref, get } from 'firebase/database';
-import { getStorage, ref as storageRef, getDownloadURL } from 'firebase/storage';
-import standardImage from './img/standard.jpg';
-import hubImage from './img/hub.jpg';
-import nolanImage from './img/nolan.jpg';
-import kelseyImage from './img/kelsey.jpg';
-import twelveImage from './img/twelve.jpg';
+import { getStorage, ref as storageRef, getDownloadURL, getMetD} from 'firebase/storage';
 
-const imageArray = {
-  standard: standardImage,
-  hub: hubImage,
-  nolan: nolanImage,
-  kelsey: kelseyImage,
-  twelve: twelveImage
-};
 
 function ApartmentDetail  ()  {
   const [apartmentData, setApartmentData] = useState(null);
@@ -33,15 +21,7 @@ function ApartmentDetail  ()  {
 
         const storage = getStorage();
         const imageRef = storageRef(storage, `apartment-images/${id}`);
-
-        try {
-          const imageUrl = await getDownloadURL(imageRef);
-          setApartmentImage(imageUrl);
-        } catch (error) {
-          console.error('Error fetching image:', error.message);
-        }
-      } else {
-        console.log('No data available');
+        console.log('imageRef:', imageRef);
       }
     };
 
@@ -65,21 +45,6 @@ function ApartmentDetail  ()  {
     window.location.href = `mailto:${ownerEmail}`;
   };
 
-  const getImage = () => {
-    if (apartmentData && apartmentData.image) {
-      return apartmentData.image;
-    } else {
-      const defaultImages = {
-        standard: standardImage,
-        hub: hubImage,
-        nolan: nolanImage,
-        kelsey: kelseyImage,
-        twelve: twelveImage
-      };
-      return defaultImages[apartmentData.id] || standardImage;
-    }
-  };
-
 
 
   return (
@@ -88,7 +53,7 @@ function ApartmentDetail  ()  {
       <p className="address">{apartmentData.address}</p>
       </div>
       <div>
-        <img src={apartmentImage || getImage()} alt="Apartment Picture" />
+        <img src={apartmentData.image} alt="Apartment Picture" />
       </div>
       <div>
         <h3>Bedrooms:</h3>
